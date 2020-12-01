7be16ae8a5084a598747e70d206cd6d77322ab7d
Andre LaFleur <alafleur@mavenlink.com>
2020-12-01 14:55:15 -0700

    Get first bit figured out

--- lib/AdventOfCode2020/day_one.rb
+++ lib/AdventOfCode2020/day_one.rb
@@ -1,4 +1,25 @@
 class DayOne
-	def initialize
+	attr_reader :list
+
+	def initialize(list)
+		@list = list
+	end
+
+	def numbers
+		current_idx = 0
+		to_test = 1
+
+		while to_test < list.size do
+			a = list[current_idx]
+			b = list[to_test]
+
+			if a + b == 2020
+				return [a, b]
+			else
+				to_test += 1
+			end
+		end
+
+		return nil
 	end
 end
--- test/day_one_test.rb
+++ test/day_one_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 describe 'DayOne' do
-	it 'initializes' do
-		assert DayOne.new
+	it 'finds two numbers that sum to 2020' do
+		numbers = [1721, 979, 366, 299, 675, 1456]
+		day = DayOne.new(numbers)
+		assert day.numbers == [1721, 299]
 	end
 end
