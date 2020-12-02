daff55de309c1b32ee3c0861ab12c48fcafc4282
U-DESKTOP-6JD46NC\alafleur <Andre LaFleur>
2020-12-01 21:45:19 -0700

    Perform class abstraction

--- lib/AdventOfCode2020/day_one.rb
+++ lib/AdventOfCode2020/day_one.rb
@@ -10,7 +10,7 @@ class DayOne
 	def numbers
 		return @numbers unless @numbers.nil?
 
-		meta_search(0)
+		NumberSearch.new(self).meta_search(0)
 		current_values
 	end
 
@@ -18,8 +18,6 @@ class DayOne
 		@numbers ||= numbers
 		@numbers.inject(:*)
 	end
-
-	private
 
 	def indexes
 		@indexes ||= (0...amount_of_addends).to_a
@@ -29,35 +27,45 @@ class DayOne
 		indexes.map { |i| list[i] }
 	end
 
-	def meta_search(index)
-		while indexes[index] < list.size do
-			if indexes[index + 1] == indexes[-1]
-				did_find = search_within(index + 1) { current_values.inject(:+) == 2020 }
-			else
-				did_find = meta_search(index + 1)
-			end
+	private
 
-			if did_find
-				@numbers = current_values
-				return true
-			end
+	class NumberSearch
+		attr_reader :day
 
-			indexes[index] += 1
-			indexes[index + 1] = indexes[index] + 1
+		def initialize(day)
+			@day = day
 		end
 
-		false
-	end
+		def meta_search(index)
+			while day.indexes[index] < day.list.size do
+				if day.indexes[index + 1] == day.indexes[-1]
+					did_find = search_within(index + 1) { day.current_values.inject(:+) == 2020 }
+				else
+					did_find = meta_search(index + 1)
+				end
 
-	def search_within(index)
-		while indexes[index] < list.size do
-			if yield
-				return true
-			else
-				indexes[index] += 1
+				if did_find
+					@numbers = day.current_values
+					return true
+				end
+
+				day.indexes[index] += 1
+				day.indexes[index + 1] = day.indexes[index] + 1
 			end
+
+			false
 		end
 
-		false
+		def search_within(index)
+			while day.indexes[index] < day.list.size do
+				if yield
+					return true
+				else
+					day.indexes[index] += 1
+				end
+			end
+
+			false
+		end
 	end
 end
