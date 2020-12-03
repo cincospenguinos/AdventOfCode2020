b14037dd0dfd64a1b8930635061d75aa40ac7a34
Andre LaFleur <alafleur@mavenlink.com>
2020-12-02 22:15:58 -0700

    Finish day 3

--- lib/AdventOfCode2020/day_three.rb
+++ lib/AdventOfCode2020/day_three.rb
@@ -1,8 +1,9 @@
 class DayThree
-	attr_reader :map, :current_pos
+	attr_reader :map, :current_pos, :movement
 
-	def initialize(map)
+	def initialize(map, movement = { right: 3, down: 1 })
 		@map = map.split("\n").map(&:chomp)
+		@movement = movement
 		@current_pos = { x: 0, y: 0 }
 	end
 
@@ -12,9 +13,9 @@ class DayThree
 		while current_pos[:y] < map.length
 			row = map[current_pos[:y]]
 			trees += 1 if row[current_pos[:x]] == '#'
-			current_pos[:x] += 3
+			current_pos[:x] += movement[:right]
 			current_pos[:x] %= row.length
-			current_pos[:y] += 1
+			current_pos[:y] += movement[:down]
 		end
 
 		trees
--- test/day_three_test.rb
+++ test/day_three_test.rb
@@ -23,4 +23,20 @@ describe 'DayThree' do
 		map = File.read('data/day3.txt')
 		assert DayThree.new(map).trees == 276
 	end
+
+	it 'accepts a second movement arg' do
+		assert DayThree.new(example_map, { right: 1, down: 1 }).trees == 2
+	end
+
+	it 'solves part two' do
+		map = File.read('data/day3.txt')
+		assert [
+			{ right: 1, down: 1 },
+			{ right: 3, down: 1 },
+			{ right: 5, down: 1 },
+			{ right: 7, down: 1 },
+			{ right: 1, down: 2 }
+		].map { |movement| DayThree.new(map, movement).trees }
+			.inject(:*) == 7812180000
+	end
 end
