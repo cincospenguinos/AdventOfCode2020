5377c4977f0d2ed947bf17bb2190133995351da9
Andre LaFleur <andre.lafleur@utah.edu>
2020-12-06 18:34:16 -0700

    Handle part one all the way

--- test/day_six_test.rb
+++ test/day_six_test.rb
@@ -5,4 +5,14 @@ describe 'DaySix' do
 		groups = ['abc']
 		assert DaySix.new(groups).count == 3
 	end
+
+	it 'handles a group of two' do
+		groups = ['a ab']
+		assert DaySix.new(groups).count == 2
+	end
+
+	it 'handles a group of three' do
+		groups = ['a ab cdg']
+		assert DaySix.new(groups).count == 5
+	end
 end
